f222daee0ea9a3d841c0992c54aa9d5ffecb66cb
Nicholas Sanchez <nicholas.sanchez@wizeline.com>
2018-07-22 21:13:17 -0700

    Committing files

---
+++ DiaryWebApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class DiaryWebApp extends Component{
+  constructor(props) {
+  }
+
+
+  render() {
+    return {
+      <div>
+        Hello world
+      </div>
+    }
+  }
+}
